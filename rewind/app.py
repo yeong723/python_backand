8b641dd9c210e9115153aeef711d521d10da57ec
young723 <sgagjq132@gmail.com>
2024-05-24 14:45:59 +0900

    feat: post

--- rewind/app.py
+++ rewind/app.py
@@ -12,6 +12,22 @@ def signup():
     app.users[app.idCnt] = newUser
     app.idCnt += 1
     return jsonify(newUser)
+@app.route('/post', methods=['POST'])
+def post():
+    payload = request.json
+    userID = int(payload['ID'])
+    msg = payload['msg']
+
+    if userID not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+    if len(msg) > 300:
+        return '300자를 초과했습니다', 400
+    
+    app.posts.append({
+        'user_id' : userID,
+        'post' : msg
+    })
+    return '성공', 200
 
 if __name__ == '__main__':
     app.run()
